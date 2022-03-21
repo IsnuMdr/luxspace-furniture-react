4955156350137520d0bcf52a0876a263f510814f
Isnu Munandar <isnu.m11@gmail.com>
2022-03-21 11:58:00 +0700

    add useScrollAnchor, useModalDOM

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "partials/Header";
-import Hero from "partials/Hero";
+import Hero from "partials/HomePage/Hero";
 import BrowseRoom from "partials/HomePage/BrowseRoom";
 import JustArrived from "partials/HomePage/JustArrived";
 import Clients from "partials/Clients";
 import Sitemap from "partials/Sitemap";
 import Footer from "partials/Footer";
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+import useModalDOM from "helpers/hooks/useModalDOM";
 
 export default function HomePage() {
+  useScrollAnchor();
+  useModalDOM();
   return (
     <>
       <Header theme="white" position="absolute" />
